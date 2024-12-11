a7c43b242e1fbdebaf55ddebfeeaa60a397cb065
ahmed-glitch07 <pashaahmed162@gmail.com>
2024-12-11 19:17:47 +0530

    vercel updates1

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -27,7 +27,7 @@ exports.login = async (req, res) => {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
 
-    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET || 'jwtsecret_key_here', { expiresIn: '1d' });
     res.status(200).json({ token, user });
   } catch (error) {
     res.status(400).json({ error: error.message });
